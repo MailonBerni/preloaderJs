6df33be771d805edc2a11afa36c3fbab26b63187
MailonBerni <165777995+MailonBerni@users.noreply.github.com>
2024-04-29 08:55:02 -0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -16,7 +16,7 @@ overlay.style.cssText = `
 
 const svgImage = document.createElement('img');
 svgImage.id = 'svgImage';
-svgImage.src = 'https://jvsc99.github.io/preloaderJs/imagem.svg';
+svgImage.src = 'https://mailonberni.github.io/preloaderJs/logo-azul-preta.png';
 svgImage.style.cssText = `
     max-width: 100%;
     max-height: 100%;
